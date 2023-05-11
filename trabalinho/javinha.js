f8fba24549fba6db7c988d1b0a78632e8f0a86bd
viniloko2 <114232800+viniloko2@users.noreply.github.com>
2023-05-10 21:32:43 -0300

    Add files via upload

--- trabalinho/javinha.js
+++ trabalinho/javinha.js
@@ -1,3 +1,15 @@
 function salvar(){
-    let tableUser = document.querySelector('table'); 
+    let tableUser = document.querySelector('.table'); 
+    var row = tableUser.insertRow();
+
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
+  cell1.innerHTML = document.getElementById('nome').value;
+  cell2.innerHTML = document.getElementById('email').value;
+  cell3.innerHTML = document.getElementById('data').value;
+  cell4.innerHTML = document.getElementById('estado').value;
+
 }
